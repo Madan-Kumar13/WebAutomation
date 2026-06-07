48137b8a3a883ae504682dd44eef42924c5aa8ab
Madan-Kumar13 <madankumarsp969@gmail.com>
2026-06-07 15:00:21 +0530

    Added TestNG suite and WebDriverManager

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -7,26 +7,28 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
 
 	protected WebDriver driver;
-	
+
 	@BeforeMethod
 	public void setup() {
+
+		WebDriverManager.chromedriver().setup();
+
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 		driver.get("https://www.saucedemo.com");
-		
 	}
-	
+
 	@AfterMethod
-	public void close()
-	{
-		if(driver!= null)
-		{
-			//driver.quit();
+	public void close() {
+
+		if (driver != null) {
+			driver.quit();
 		}
 	}
-	
 }
